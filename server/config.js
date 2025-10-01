983280c0a8dc00695d57fc1990be24c8b45791cd
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-10-01 00:43:28 -0300

    agregar conexión a la base de datos MongoDB en config.js y actualizar la configuración del servidor para incluirla

--- server/config.js
+++ server/config.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import path from "path";
 import { fileURLToPath } from "url";
+import "../db/config.js";
 
 export default class Server {
   constructor() {
