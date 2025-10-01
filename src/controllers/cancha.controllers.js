221ff6037def1fcb9fcad353ba378b92450d5b6c
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-10-01 10:58:48 -0300

    agregar la lógica para crear canchas en el controlador y actualizar las rutas correspondientes

--- src/controllers/cancha.controllers.js
+++ src/controllers/cancha.controllers.js
@@ -4,3 +4,12 @@ export const prueba= (req, res) => {
     res.status(200)
     res.send("Mensaje desde el controlador de canchas")
 }
+
+export const crearCancha= async(req, res) => {
+    try {
+        console.log(req.body)
+        res.send("desde la logica de crear producto")
+    } catch (error) {
+        console.error(error)
+    }
+}
--- src/routes/cancha.routes.js
+++ src/routes/cancha.routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { prueba } from "../controllers/cancha.controllers.js";
+import { crearCancha, prueba } from "../controllers/cancha.controllers.js";
 
 const router = Router();
 
 router.route("/prueba").get(prueba);
+router.route("/").post(crearCancha);
 
 export default router;
