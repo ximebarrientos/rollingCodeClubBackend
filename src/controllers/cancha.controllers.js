e6f85e95929bb1ef212170e66f7cc2705086b0d4
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-10-03 20:06:21 -0300

    agregue funcion obtener una cancha por ID y la ruta necesaria

--- src/controllers/cancha.controllers.js
+++ src/controllers/cancha.controllers.js
@@ -1,29 +1,40 @@
-import Cancha from "../models/cancha.js"
+import Cancha from "../models/cancha.js";
 
+export const prueba = (req, res) => {
+  res.status(200);
+  res.send("Mensaje desde el controlador de canchas");
+};
 
+export const crearCancha = async (req, res) => {
+  try {
+    const canchaNueva = new Cancha(req.body);
+    await canchaNueva.save();
+    res.status(201).json({ mensaje: "Cancha creada con exito" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al crear la cancha" });
+  }
+};
 
-export const prueba= (req, res) => {
-    res.status(200)
-    res.send("Mensaje desde el controlador de canchas")
-}
+export const obtenerCanchas = async (req, res) => {
+  try {
+    const listaCanchas = await Cancha.find();
+    res.status(200).json(listaCanchas);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al obtener las canchas" });
+  }
+};
 
-export const crearCancha= async(req, res) => {
-    try {
-        const canchaNueva= new Cancha(req.body);
-        await canchaNueva.save();
-        res.status(201).json({mensaje:"Cancha creada con exito"})
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({mensaje:"Error al crear la cancha"})
+export const obtenerCanchaPorId = async (req, res) => {
+  try {
+    const canchaBuscada = await Cancha.findById(req.params.id);
+    if (!canchaBuscada) {
+      return res.status(404).json({ mensaje: "Cancha no encontrada" });
     }
-}
-
-export const obtenerCanchas= async(req, res) => {
-    try {
-        const listaCanchas= await Cancha.find();
-        res.status(200).json(listaCanchas)
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({mensaje:"Error al obtener las canchas"})
-    }
-}
+    res.status(200).json(canchaBuscada);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ mensaje: "Error al obtener la cancha por ID" });
+  }
+};
--- src/routes/cancha.routes.js
+++ src/routes/cancha.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { crearCancha, obtenerCanchas, prueba } from "../controllers/cancha.controllers.js";
+import { crearCancha, obtenerCanchaPorId, obtenerCanchas, prueba } from "../controllers/cancha.controllers.js";
 
 const router = Router();
 
 router.route("/prueba").get(prueba);
 router.route("/").post(crearCancha).get(obtenerCanchas);
+router.route("/:id").get(obtenerCanchaPorId)
 
 export default router;
