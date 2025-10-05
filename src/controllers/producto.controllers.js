94f19f8f7049f0cea91d70646ca0c59bfb0f638e
Alessandra Borges <aleborges110900@gmail.com>
2025-10-05 02:55:57 -0300

    Agregue el controlador de obtener productos

--- src/controllers/producto.controllers.js
+++ src/controllers/producto.controllers.js
@@ -11,11 +11,22 @@ export const crearProducto= async(req, res) => {
        
         const productoNuevo = new Producto(req.body)
         await productoNuevo.save()
-        //mensaje de respuesta
+      
         res.status(201).json({mensaje:"Producto creado con exito"});
         
     } catch (error) {
         console.error(error)
         res.status(500).json({mensaje:"Error al crear el producto", error:error.message})
+    }
+}
+
+export const obtenerProductos= async(req, res) => {
+    try {
+        const listaProductos = await Producto.find();
+        res.status(200).json(listaProductos);
+        
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({mensaje:"Error al obtener los productos", error:error.message})
     }
 }
--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearProducto, prueba } from "../controllers/producto.controllers.js";
+import { crearProducto, obtenerProductos, prueba } from "../controllers/producto.controllers.js";
 
 const router = Router();
 
 router.route("/prueba").get(prueba);
-router.route("/").post(crearProducto);
+router.route("/").post(crearProducto).get(obtenerProductos);
 
 export default router;
