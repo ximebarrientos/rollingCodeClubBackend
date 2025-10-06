9fa8ee72a2aeffba379152e31cea61740b83d94c
Alessandra Borges <aleborges110900@gmail.com>
2025-10-06 04:18:58 -0300

    Cree las validaciones de productos

--- src/routes/producto.routes.js
+++ src/routes/producto.routes.js
@@ -1,10 +1,24 @@
 import { Router } from "express";
-import { crearProducto, obtenerProductos, prueba, obtenerProductoPorId, borrarProductoPorId, editarProductoPorId } from "../controllers/producto.controllers.js";
+import {
+  crearProducto,
+  obtenerProductos,
+  prueba,
+  obtenerProductoPorId,
+  borrarProductoPorId,
+  editarProductoPorId,
+} from "../controllers/producto.controllers.js";
+import validacionProducto from "../middleware/validarProducto.js";
 
 const router = Router();
 
 router.route("/prueba").get(prueba);
-router.route("/").post(crearProducto).get(obtenerProductos);
-router.route("/:id").get(obtenerProductoPorId).delete(borrarProductoPorId).put(editarProductoPorId);
+
+router.route("/").post(validacionProducto, crearProducto).get(obtenerProductos);
+
+router
+  .route("/:id")
+  .get(obtenerProductoPorId)
+  .delete(borrarProductoPorId)
+  .put(validacionProducto, editarProductoPorId);
 
 export default router;
