80524acef5dbabe2e50e5c85819f31d9aa201198
Claudia Ximena Barrientos <cxbarrientos1@gmail.com>
2025-10-13 21:12:57 -0300

    agregue la validacion de la cancha por ID y verificar token en las peticiones de las canchas

--- src/routes/cancha.routes.js
+++ src/routes/cancha.routes.js
@@ -2,11 +2,12 @@ import { Router } from "express";
 import { borrarCanchaPorID, crearCancha, editarCanchaPorID, obtenerCanchaPorId, obtenerCanchas, prueba } from "../controllers/cancha.controllers.js";
 import validacionCancha from "../middleware/validarCancha.js";
 import verificarToken from "../middleware/verificarToken.js";
+import validarIdCancha from "../middleware/validarIdCancha.js";
 
 const router = Router();
 
 router.route("/prueba").get(prueba);
 router.route("/").post([verificarToken,validacionCancha],crearCancha).get(obtenerCanchas);
-router.route("/:id").get(obtenerCanchaPorId).delete(borrarCanchaPorID).put(validacionCancha,editarCanchaPorID)
+router.route("/:id").get(validarIdCancha,obtenerCanchaPorId).delete([verificarToken,validarIdCancha],borrarCanchaPorID).put([verificarToken, validarIdCancha,validacionCancha],editarCanchaPorID)
 
 export default router;
