655b89f913e30f3d0e6d5213c7188aee2d30c7a3
Alessandra Borges <aleborges110900@gmail.com>
2025-10-14 06:52:51 -0300

    Agregué cloudinary a canchas y solucioné problemas de validaciones

--- src/routes/cancha.routes.js
+++ src/routes/cancha.routes.js
@@ -3,11 +3,13 @@ import { borrarCanchaPorID, crearCancha, editarCanchaPorID, obtenerCanchaPorId, 
 import validacionCancha from "../middleware/validarCancha.js";
 import verificarToken from "../middleware/verificarToken.js";
 import validarIdCancha from "../middleware/validarIdCancha.js";
+import upload from "../helpers/upload.js";
+import errorMulter from "../middleware/errorMulter.js";
 
 const router = Router();
 
 
-router.route("/").post([verificarToken,validacionCancha],crearCancha).get(obtenerCanchas);
-router.route("/:id").get(validarIdCancha,obtenerCanchaPorId).delete([verificarToken,validarIdCancha],borrarCanchaPorID).put([verificarToken, validarIdCancha,validacionCancha],editarCanchaPorID)
+router.route("/").post([verificarToken, upload, errorMulter, validacionCancha],crearCancha).get(obtenerCanchas);
+router.route("/:id").get(validarIdCancha,obtenerCanchaPorId).delete([verificarToken,validarIdCancha],borrarCanchaPorID).put([verificarToken, validarIdCancha, upload, errorMulter, validacionCancha],editarCanchaPorID)
 
 export default router;
